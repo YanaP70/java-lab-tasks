40733fc6202b47fda24deeecc2b0fc94f7f79382
Yana Podoliak <polyak@ua.fm>
2022-11-18 23:33:33 +0200

    code fixed

--- src/main/java/task01/cow/CowList.java
+++ src/main/java/task01/cow/CowList.java
@@ -1,22 +1,23 @@
 package task01.cow;
 
 import java.util.*;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 
-public class CowList<E> extends AbstractList<E> {
-    private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
-    private final Lock readLock = lock.readLock();
-    private final Lock writeLock = lock.writeLock();
+public class CowList extends AbstractList<Model> {
+    private final ReentrantLock lock = new ReentrantLock();
 
-    private volatile Object[] array = {};
+    private volatile Model[] array = {};
 
-    public Object[] getArray() {
-        return array;
+    public CowList() {
     }
 
-    public void setArray(Object[] array) {
+    public CowList(Model[] array) {
+        this.array = array;
+    }
+
+    private void setArray(Model[] array) {
         this.array = array;
     }
 
@@ -32,228 +33,248 @@ public class CowList<E> extends AbstractList<E> {
 
     @Override
     public int indexOf(Object o) {
-        readLock.lock();
-        try {
-            for (int i = 0; i < size(); i++) {
-                if (array[i].equals(o)) {
-                    return i;
-                }
+        for (int i = 0; i < size(); i++) {
+            if (isSame(o, array[i])) {
+                return i;
             }
-        } finally {
-            readLock.unlock();
         }
         return -1;
     }
 
     @Override
     public int lastIndexOf(Object o) {
-        readLock.lock();
-        try {
-            for (int i = size() - 1; i >= 0; i--) {
-                if (array[i].equals(o)) {
-                    return i;
-                }
+        for (int i = size() - 1; i >= 0; i--) {
+            if (isSame(o, array[i])) {
+                return i;
             }
-        } finally {
-            readLock.unlock();
         }
         return -1;
     }
 
+    private boolean isSame(Object o, Model obj) {
+        return Objects.isNull(obj) && Objects.isNull(o) || obj.equals(o);
+    }
+
     @Override
     public boolean contains(Object o) {
-        readLock.lock();
-        boolean result;
-        try {
-            result = indexOf(o) >= 0;
-        } finally {
-            readLock.unlock();
-        }
-        return result;
+        return indexOf(o) >= 0;
     }
 
     @Override
     public boolean containsAll(Collection<?> c) {
-        readLock.lock();
-        boolean result;
-        try {
-            result = new ArrayList<>(Arrays.asList(array)).containsAll(c);
-        } finally {
-            readLock.unlock();
-        }
-        return result;
+        Objects.requireNonNull(c);
+        return c.stream().allMatch(this::contains);
     }
 
-    @SuppressWarnings("unchecked")
     @Override
-    public E[] toArray() {
-        readLock.lock();
-        E[] elements;
-        try {
-            elements = (E[]) Arrays.copyOf(array, array.length);
-        } finally {
-            readLock.unlock();
-        }
-        return elements;
+    public Model[] toArray() {
+        return Arrays.copyOf(array, array.length);
     }
 
-    @SuppressWarnings("unchecked")
     @Override
-    public E get(int index) {
-        readLock.lock();
-        E element;
-        try {
-            element = (E) array[index];
-        } finally {
-            readLock.unlock();
-        }
-        return element;
+    public Model get(int index) {
+        return array[index];
     }
 
-    @SuppressWarnings("unchecked")
     @Override
-    public E set(int index, E element) {
-        writeLock.lock();
-        E oldElement;
+    public Model set(int index, Model element) {
+        lock.lock();
         try {
-            Object[] snapshot = Arrays.copyOf(array, array.length);
-            oldElement = (E) snapshot[index];
+            Model[] snapshot = Arrays.copyOf(array, array.length);
+            Model oldElement = snapshot[index];
             snapshot[index] = element;
             setArray(snapshot);
+            return oldElement;
         } finally {
-            writeLock.unlock();
-        }
-        return oldElement;
-    }
-
-    @Override
-    public boolean add(E element) {
-        writeLock.lock();
-        try {
-            Object[] snapshot = Arrays.copyOf(array, array.length + 1);
-            snapshot[snapshot.length - 1] = element;
-            setArray(snapshot);
-        } finally {
-            writeLock.unlock();
-        }
-        return true;
-    }
-
-    @Override
-    public void add(int index, E element) {
-        writeLock.lock();
-        try {
-            Object[] snapshot = new Object[size() + 1];
-            System.arraycopy(array, 0, snapshot, 0, index);
-            System.arraycopy(array, index, snapshot, index + 1, size() - index);
-            snapshot[index] = element;
-            setArray(snapshot);
-        } finally {
-            writeLock.unlock();
+            lock.unlock();
         }
     }
 
-    @SuppressWarnings("unchecked")
     @Override
-    public E remove(int index) {
-        writeLock.lock();
-        E element;
+    public boolean add(Model element) {
+        lock.lock();
         try {
-            element = (E) getArray()[index];
-            Object[] snapshot = new Object[size() - 1];
+            setArray(addElement(size(), element));
+            return true;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    @Override
+    public void add(int index, Model element) {
+        lock.lock();
+        try {
+            setArray(addElement(index, element));
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    private Model[] addElement(int index, Model element) {
+        Model[] snapshot = new Model[size() + 1];
+        System.arraycopy(array, 0, snapshot, 0, index);
+        System.arraycopy(array, index, snapshot, index + 1, size() - index);
+        snapshot[index] = element;
+        return snapshot;
+    }
+
+    @Override
+    public Model remove(int index) {
+        lock.lock();
+        try {
+            Model element = array[index];
+            Model[] snapshot = new Model[size() - 1];
             System.arraycopy(array, 0, snapshot, 0, index);
             System.arraycopy(array, index + 1, snapshot, index, size() - 1 - index);
             setArray(snapshot);
+            return element;
         } finally {
-            writeLock.unlock();
+            lock.unlock();
         }
-        return element;
     }
 
     @Override
     public boolean remove(Object o) {
+        Objects.requireNonNull(o);
         remove(indexOf(o));
         return true;
     }
 
     @Override
     public void clear() {
-        writeLock.lock();
+        lock.lock();
         try {
-            Object[] snapshot = {};
-            setArray(snapshot);
+            setArray(new Model[0]);
         } finally {
-            writeLock.unlock();
+            lock.unlock();
         }
     }
 
     @Override
-    public boolean addAll(Collection<? extends E> c) {
-        writeLock.lock();
+    public boolean addAll(Collection<? extends Model> c) {
+        Objects.requireNonNull(c);
+        lock.lock();
         try {
-            Object[] snapshot = new Object[size() + c.size()];
-            System.arraycopy(array, 0, snapshot, 0, size());
-            System.arraycopy(c.toArray(), 0, snapshot, size(), c.size());
-            setArray(snapshot);
+            setArray(addAllElements(size(), c));
+            return true;
         } finally {
-            writeLock.unlock();
+            lock.unlock();
         }
-        return true;
     }
 
     @Override
-    public boolean addAll(int index, Collection<? extends E> c) {
-        writeLock.lock();
+    public boolean addAll(int index, Collection<? extends Model> c) {
+        Objects.requireNonNull(c);
+        lock.lock();
         try {
-            Object[] snapshot = new Object[size() + c.size()];
-            System.arraycopy(array, 0, snapshot, 0, index);
-            System.arraycopy(c.toArray(), 0, snapshot, index, c.size());
-            System.arraycopy(array, index, snapshot, c.size() + index, size() - index);
-            setArray(snapshot);
+            setArray(addAllElements(index, c));
+            return true;
         } finally {
-            writeLock.unlock();
+            lock.unlock();
         }
-        return true;
+    }
+
+    private Model[] addAllElements(int index, Collection<? extends Model> c) {
+        Model[] snapshot = new Model[size() + c.size()];
+        Model[] collection = Arrays.copyOf(c.toArray(), c.size(), Model[].class);
+
+        System.arraycopy(array, 0, snapshot, 0, index);
+        System.arraycopy(collection, 0, snapshot, index, c.size());
+        System.arraycopy(array, index, snapshot, c.size() + index, size() - index);
+        return snapshot;
     }
 
     @Override
     public boolean removeAll(Collection<?> c) {
-        writeLock.lock();
+        Objects.requireNonNull(c);
+        lock.lock();
         try {
-            List<Object> list = new ArrayList<>(Arrays.asList(array));
-            list.removeAll(c);
-            setArray(list.toArray());
+            Predicate<Model> p = c::contains;
+            setArray(removeElements(p.negate()));
+            return true;
         } finally {
-            writeLock.unlock();
+            lock.unlock();
         }
-        return true;
     }
 
     @Override
     public boolean retainAll(Collection<?> c) {
-        writeLock.lock();
+        lock.lock();
         try {
-            List<Object> list = new ArrayList<>(Arrays.asList(array));
-            list.retainAll(c);
-            setArray(list.toArray());
+            Predicate<Model> p = c::contains;
+            setArray(removeElements(p));
+            return true;
         } finally {
-            writeLock.unlock();
+            lock.unlock();
         }
-        return true;
+    }
+
+    private Model[] removeElements(Predicate<Model> predicate) {
+        Model[] snapshot = new Model[size()];
+        Iterator<Integer> in = iterator(predicate);
+        int prev = 0;
+        int last = 0;
+        while (in.hasNext()) {
+            int next = in.next();
+            System.arraycopy(array, prev, snapshot, last, next - prev);
+            last += next - prev;
+            prev = next + 1;
+        }
+        System.arraycopy(array, prev, snapshot, last, size() - prev);
+        last = last + size() - prev;
+
+        Model[] newarray = new Model[last];
+        System.arraycopy(snapshot, 0, newarray, 0, last);
+        return newarray;
+    }
+
+    private Iterator<Integer> iterator(Predicate<Model> predicate) {
+        return new Iterator<>() {
+            private final Model[] snapshot = array;
+            private int position = 0;
+
+            @Override
+            public boolean hasNext() {
+                while (position < size() && predicate.test(snapshot[position])) {
+                    position++;
+                }
+                return position < size();
+            }
+
+            @Override
+            public Integer next() {
+                if (position >= size()) {
+                    throw new NoSuchElementException();
+                }
+                return position++;
+            }
+        };
+    }
+
+    @SuppressWarnings({"unchecked", "SuspiciousSystemArraycopy"})
+    @Override
+    public <T> T[] toArray(T[] a) {
+        if (Objects.isNull(a) || a.length != size()) {
+            a = (T[]) Arrays.copyOf(array, array.length, a.getClass());
+        } else {
+            System.arraycopy(array, 0, a, 0, size());
+        }
+        return a;
     }
 
     @Override
-    public ListIterator<E> listIterator() {
+    public ListIterator<Model> listIterator() {
         throw new UnsupportedOperationException();
     }
 
     @Override
-    public ListIterator<E> listIterator(int index) {
+    public ListIterator<Model> listIterator(int index) {
         throw new UnsupportedOperationException();
     }
 
     @Override
-    public List<E> subList(int fromIndex, int toIndex) {
+    public List<Model> subList(int fromIndex, int toIndex) {
         throw new UnsupportedOperationException();
     }
 
@@ -263,14 +284,9 @@ public class CowList<E> extends AbstractList<E> {
     }
 
     @Override
-    public <T> T[] toArray(T[] a) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public Iterator<E> iterator() {
+    public Iterator<Model> iterator() {
         return new Iterator<>() {
-            private final Object[] snapshot = array;
+            private final Model[] snapshot = array;
             private int position = 0;
 
             @Override
@@ -278,12 +294,11 @@ public class CowList<E> extends AbstractList<E> {
                 return position < snapshot.length;
             }
 
-            @SuppressWarnings("unchecked")
             @Override
-            public E next() {
-                if (! hasNext())
+            public Model next() {
+                if (!hasNext())
                     throw new NoSuchElementException();
-                return (E) snapshot[position++];
+                return snapshot[position++];
             }
 
             @Override
@@ -292,9 +307,10 @@ public class CowList<E> extends AbstractList<E> {
             }
 
             @Override
-            public void forEachRemaining(Consumer<? super E> action) {
+            public void forEachRemaining(Consumer<? super Model> action) {
                 throw new UnsupportedOperationException();
             }
         };
     }
 }
+
--- src/test/java/task01/cow/CowListTest.java
+++ src/test/java/task01/cow/CowListTest.java
@@ -2,157 +2,192 @@ package task01.cow;
 
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 public class CowListTest {
-    private CowList<Integer> cowList;
 
-    @Before
-    public void setUp() {
-        cowList = new CowList<>();
-        cowList.addAll(List.of(1, 2, 3, 4, 5));
-    }
+    private final Collection<Model> models = List.of(
+            new Model(2, "2"),
+            new Model(3, "3"),
+            new Model(5, "5"));
 
-    @Test
-    public void getArray() {
-        Assert.assertArrayEquals(new Object[]{1, 2, 3, 4, 5}, cowList.getArray());
-        Assert.assertEquals(5, cowList.getArray().length);
-    }
+    private final Model[] arr = {
+            new Model(1, "1"),
+            new Model(2, "2"),
+            new Model(3, "3"),
+            new Model(4, "4"),
+            new Model(5, "5"),
+            new Model(6, "6")};
 
-    @Test
-    public void setArray() {
-        cowList.setArray(new Object[]{9});
-        Assert.assertArrayEquals(new Object[]{9}, cowList.getArray());
-        Assert.assertEquals(1, cowList.getArray().length);
-    }
+    private CowList cowList = new CowList(arr);
 
     @Test
     public void size() {
-        Assert.assertEquals(5, cowList.size());
+        Assert.assertEquals(6, cowList.size());
     }
 
     @Test
     public void isEmpty() {
-        cowList.setArray(new Object[]{});
+        cowList = new CowList();
+        Assert.assertTrue(cowList.isEmpty());
+
+        cowList = new CowList(new Model[0]);
         Assert.assertTrue(cowList.isEmpty());
     }
 
     @Test
     public void indexOf() {
-        Assert.assertEquals(0, cowList.indexOf(1));
+        cowList.add(new Model(6, "6"));
+        Assert.assertEquals(5, cowList.indexOf(new Model(6, "6")));
     }
 
     @Test
     public void indexOfNotFound() {
-        Assert.assertEquals(-1, cowList.indexOf(0));
+        Assert.assertEquals(-1, cowList.indexOf(new Model(9, "9")));
     }
 
     @Test
     public void testLastIndexOf() {
-        cowList.setArray(new Object[]{1, 5, 2, 3, 5, 4});
-        Assert.assertEquals(4, cowList.lastIndexOf(5));
+        cowList.add(new Model(6, "6"));
+        Assert.assertEquals(6, cowList.lastIndexOf(new Model(6, "6")));
     }
 
     @Test
     public void testLastIndexOfNotFound() {
-        Assert.assertEquals(-1, cowList.lastIndexOf(0));
+        Assert.assertEquals(-1, cowList.lastIndexOf(new Model(7, "7")));
     }
 
     @Test
     public void contains() {
-        Assert.assertTrue(cowList.contains(1));
-        Assert.assertFalse(cowList.contains(0));
+        Assert.assertTrue(cowList.contains(new Model(5, "5")));
+        Assert.assertFalse(cowList.contains(new Model(9, "9")));
     }
 
     @Test
     public void containsAll() {
-        Assert.assertTrue(cowList.containsAll(List.of(1, 2)));
-        Assert.assertFalse(cowList.containsAll(List.of(0, 1)));
+        Assert.assertTrue(cowList.containsAll(List.of(new Model(5, "5"), new Model(6, "6"))));
+        Assert.assertFalse(cowList.containsAll(List.of(new Model(7, "7"), new Model(6, "6"))));
+        Assert.assertFalse(cowList.containsAll(List.of(new Model(7, "7"), new Model(8, "8"))));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void containsAllNegative() {
+        Assert.assertFalse(cowList.containsAll(null));
     }
 
     @Test
     public void toArray() {
-        Assert.assertArrayEquals(new Integer[]{1, 2, 3, 4, 5}, cowList.toArray());
+        Assert.assertArrayEquals(arr, cowList.toArray());
     }
 
     @Test
     public void get() {
-        Assert.assertEquals(Integer.valueOf(3), cowList.get(2));
+        Assert.assertEquals(new Model(3, "3"), cowList.get(2));
     }
 
     @Test
     public void set() {
-        cowList.set(2, -3);
-        Assert.assertArrayEquals(new Integer[]{1, 2, -3, 4, 5}, cowList.getArray());
+        cowList.set(2, new Model(22, "22"));
+        Assert.assertEquals(new Model(22, "22"), cowList.get(2));
     }
 
     @Test
     public void add() {
-        cowList.add(6);
-        Assert.assertArrayEquals(new Integer[]{1, 2, 3, 4, 5, 6}, cowList.getArray());
+        cowList = new CowList();
+        Assert.assertTrue(cowList.add(new Model(1, "1")));
+        Assert.assertTrue(cowList.add(new Model(2, "2")));
+        Assert.assertTrue(cowList.add(new Model(3, "3")));
+        Assert.assertTrue(cowList.add(new Model(4, "4")));
+        Assert.assertTrue(cowList.add(new Model(5, "5")));
+        Assert.assertTrue(cowList.add(new Model(6, "6")));
+        Assert.assertArrayEquals(arr, cowList.toArray());
     }
 
     @Test
     public void testAdd() {
-        cowList.add(5, 6);
-        Assert.assertArrayEquals(new Integer[]{1, 2, 3, 4, 5, 6}, cowList.getArray());
+        cowList = new CowList();
+        cowList.add(0, new Model(6, "6"));
+        cowList.add(0, new Model(5, "5"));
+        cowList.add(0, new Model(4, "4"));
+        cowList.add(0, new Model(3, "3"));
+        cowList.add(0, new Model(2, "2"));
+        cowList.add(0, new Model(1, "1"));
+        Assert.assertArrayEquals(arr, cowList.toArray());
     }
 
     @Test
     public void remove() {
-        cowList.remove(4);
-        Assert.assertArrayEquals(new Integer[]{1, 2, 3, 4}, cowList.getArray());
+        Assert.assertTrue(cowList.remove(new Model(1, "1")));
+        Assert.assertTrue(cowList.remove(new Model(2, "2")));
+        Assert.assertTrue(cowList.remove(new Model(3, "3")));
+        Assert.assertTrue(cowList.remove(new Model(4, "4")));
+        Assert.assertTrue(cowList.remove(new Model(5, "5")));
+        Assert.assertArrayEquals(new Model[]{new Model(6, "6")}, cowList.toArray());
     }
 
     @Test
     public void testRemove() {
-        cowList.remove(Integer.valueOf(5));
-        Assert.assertArrayEquals(new Integer[]{1, 2, 3, 4}, cowList.getArray());
+        Assert.assertEquals(new Model(5, "5"), cowList.remove(4));
+        Assert.assertEquals(new Model(4, "4"), cowList.remove(3));
+        Assert.assertEquals(new Model(3, "3"), cowList.remove(2));
+        Assert.assertEquals(new Model(2, "2"), cowList.remove(1));
+        Assert.assertEquals(new Model(1, "1"), cowList.remove(0));
+        Assert.assertArrayEquals(new Model[]{new Model(6, "6")}, cowList.toArray());
     }
 
     @Test
     public void clear() {
         cowList.clear();
-        Assert.assertArrayEquals(new Integer[]{}, cowList.getArray());
+        Assert.assertArrayEquals(new Model[]{}, cowList.toArray());
     }
 
     @Test
     public void addAll() {
-        cowList.addAll(List.of(6, 7, 8, 9));
-        Assert.assertArrayEquals(new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9}, cowList.getArray());
+        cowList = new CowList();
+        cowList.addAll(Arrays.asList(arr));
+        Assert.assertArrayEquals(arr, cowList.toArray());
     }
 
     @Test
     public void testAddAll() {
-        cowList.addAll(3, List.of(6, 7, 8, 9));
-        Assert.assertArrayEquals(new Integer[]{1, 2, 3, 6, 7, 8, 9, 4, 5}, cowList.getArray());
+        cowList = new CowList();
+        cowList.addAll(0, List.of(new Model(1, "1")));
+        cowList.addAll(1, models);
+        cowList.addAll(3,List.of(new Model(4, "4")));
+        cowList.addAll(5, List.of(new Model(6, "6")));
+        Assert.assertArrayEquals(arr, cowList.toArray());
     }
 
     @Test
     public void removeAll() {
-        cowList.removeAll(List.of(2, 3));
-        Assert.assertArrayEquals(new Integer[]{1, 4, 5}, cowList.getArray());
+        cowList.removeAll(models);
+        Assert.assertArrayEquals(new Model[]{new Model(1, "1"), new Model(4, "4"), new Model(6, "6")}, cowList.toArray());
+        cowList.removeAll(List.of(new Model(1, "1")));
+        Assert.assertArrayEquals(new Model[]{new Model(4, "4"), new Model(6, "6")}, cowList.toArray());
+        cowList.removeAll(List.of(new Model(4, "4")));
+        Assert.assertArrayEquals(new Model[]{new Model(6, "6")}, cowList.toArray());
     }
 
     @Test
     public void retainAll() {
-        cowList.retainAll(List.of(2, 3));
-        Assert.assertArrayEquals(new Integer[]{2, 3}, cowList.getArray());
+        cowList.retainAll(models);
+        Assert.assertArrayEquals(models.toArray(), cowList.toArray());
     }
 
     @Test
     public void iterator() throws InterruptedException {
-        Iterator<Integer> iterator = cowList.iterator();
+
+        Iterator<Model> iterator = cowList.iterator();
         Thread thAdd = new Thread(() -> {
             Random rand = new Random();
             for (int i = 0; i < 10000; i++) {
                 try {
                     TimeUnit.MILLISECONDS.sleep(1);
-                    cowList.add(rand.nextInt(cowList.size()), i);
+                    int next = rand.nextInt(cowList.size());
+                    cowList.add(next, new Model(next, String.valueOf(next)));
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -175,31 +210,25 @@ public class CowListTest {
         thAdd.join();
         thRem.join();
 
-        Assert.assertArrayEquals(new Object[]{}, cowList.getArray());
-
-        List<Integer> list = new ArrayList<>();
-        while (iterator.hasNext()) {
-            list.add(iterator.next());
-        }
-        Assert.assertArrayEquals(new Object[]{1, 2, 3, 4, 5}, list.toArray());
+        Assert.assertArrayEquals(new Model[]{}, cowList.toArray());
     }
 
     @Test(expected = UnsupportedOperationException.class)
     public void iteratorRemove() {
-        Iterator<Integer> iterator = cowList.iterator();
+        Iterator<Model> iterator = cowList.iterator();
         iterator.remove();
     }
 
     @Test(expected = UnsupportedOperationException.class)
     public void iteratorForEach() {
-        Iterator<Integer> iterator = cowList.iterator();
-        List<Integer> list = new ArrayList<>();
-        iterator.forEachRemaining(el -> list.add(el + 1));
+        Iterator<Model> iterator = cowList.iterator();
+        List<Model> list = new ArrayList<>();
+        iterator.forEachRemaining(list::add);
     }
 
     @Test(expected = NoSuchElementException.class)
     public void iteratorNegative() {
-        Iterator<Integer> iterator = cowList.iterator();
+        Iterator<Model> iterator = cowList.iterator();
         while (iterator.hasNext()) {
             iterator.next();
         }
@@ -226,8 +255,15 @@ public class CowListTest {
         cowList.removeRange(1, 3);
     }
 
-    @Test(expected = UnsupportedOperationException.class)
-    public void testTestToArray() {
+    @Test(expected = ArrayStoreException.class)
+    public void testToArrayNegative() {
         cowList.toArray(new Integer[25]);
+    }
+
+    @Test
+    public void testToArray() {
+        Assert.assertArrayEquals(arr, cowList.toArray(new Model[arr.length -1]));
+        Assert.assertArrayEquals(arr, cowList.toArray(new Model[arr.length]));
+        Assert.assertArrayEquals(arr, cowList.toArray(new Model[arr.length + 1]));
     }
 }
